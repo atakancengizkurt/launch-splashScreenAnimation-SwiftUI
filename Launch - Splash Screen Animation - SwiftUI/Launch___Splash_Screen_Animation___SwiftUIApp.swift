5bb28019b8498ad09c0c50377b000e6fd831b815
Atakan Cengiz KURT <atakan.kurt@loodos.com>
2021-05-16 14:12:13 +0300

    Initial Commit

---
+++ Launch - Splash Screen Animation - SwiftUI/Launch___Splash_Screen_Animation___SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launch___Splash_Screen_Animation___SwiftUIApp.swift
+//  Launch - Splash Screen Animation - SwiftUI
+//
+//  Created by Atakan Cengiz KURT on 16.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Launch___Splash_Screen_Animation___SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
